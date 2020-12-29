8268f12fd7b664171a386f3083f2a1edba53c7f7
Pushkar Mehendale <mehenpm@gmail.com>
2020-12-29 15:25:00 -0600

    Update 12/29/20

--- Scrapy/quotes_spider.py
+++ Scrapy/quotes_spider.py
@@ -5,16 +5,15 @@ class QuotesSpider(scrapy.Spider):
 
 	def start_request(self):
 		urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
+		'http://quotes.toscrape.com/page/1/',
+		'http://quotes.toscrape.com/page/2/'
+		]
+		for url in urls:
+			yield scrapy.Request(url=url, callback=self.parse)
 
-        for url in urls:
-        	yield scrapy.Requests(url=url, callback=self.parse)
-
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+	def parse(self, response):
+		page = response.url.split("/")[-2]
+		filename = f'quotes-{page}.html'
+		with open(filename, 'wb') as f:
+			f.write(response.body)
+		self.log(f'Saved file {filename}')
